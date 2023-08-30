869d4e06ed062c179c9b73bb7f71a6798b5fd2a8
Юра Цепліцький <yurii.tseplitskyi.kn.2021@lpnu.ua>
2023-08-30 16:32:17 +0300

    second commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/YuriiTseplitskyi/studyingGO.git
+
+go 1.20
+
+require github.com/YuriiTseplitskyi/puppy v0.0.0-20230830132009-94b948173b16 //direct
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"github.com/YuriiTseplitskyi/puppy"
+)
+
+func main(){
+	fmt.Println("Hello World2")
+	s1:=puppy.Barks()
+	fmt.Println(s1)
+
+}
